240c8510ed89937985a9663abc07449b4df789d0
Akash9888 <62830132+Akash9888@users.noreply.github.com>
2021-01-03 10:13:25 +0600

    Create CodeChef- JAN21C - DIVTHREE.cpp

---
+++ CodeChef- JAN21C - DIVTHREE.cpp
@@ -0,0 +1,44 @@
+/*
+    Author Name: Akash Chanda Tushar(CSE,LU)
+*/
+#include<bits/stdc++.h>
+#define   ll            long long int
+#define   ull           unsigned long long
+#define   endl          "\n"
+#define   vi            vector<int>
+#define   vll           vector<ll>
+#define   vpi           vector<pair<int,int>>
+#define   vpll          vector<pair<ll,ll>>
+#define   pb            push_back
+#define   eb            emplace_back
+#define   all(p)        p.begin(),p.end()
+#define   F             first
+#define   S             second
+#define   mp            make_pair
+#define   MOD           1000000007
+using namespace std;
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+ll lcm(ll a, ll b){return (a / gcd(a, b)) * b;}
+
+
+void solve(){
+        ll n,k,d,a,sum=0;
+        cin>>n>>k>>d;
+
+        for(int i=0;i<n;i++){cin>>a;sum+=a;}
+
+        ll x=sum/k;
+        if(x<=d)cout<<x<<endl;
+        else cout<<d<<endl;
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+return 0;
+}
